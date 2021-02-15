09693eee6bbd5996947aaf70bf046aff73bf07e1
Tim Clemans <realtimclemans@gmail.com>
2021-02-15 07:29:15 -0800

    added to README and model

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# EmergencyDotHelp
-An advanced emergency communications platform
+# Emergency.help
+
+The mission of Emergency.help is to save lives and secure property. Our current focus is developing CrisisProfile and automatically dispatching first responders instantly when an emergency is reported. We will provide them the data, photographs, and video they need. Our long-term vision is to provide a combined safety, security, and health platform for all taskholders. 
+
+## Features planed
+
+### CrisisProfile
+
+This free world wide information system will provide emergency call takers and dispatchers, first responders, hospitals and clinics, schools, correctional facilities, friends and family, and employers the information they need to effectively respond to a person in a medical or mental health crisis. Unlike [Smart911](https://www.smart911.com/) and [RapidSOS's emergencyprofile.org](https://emergencyprofile.org) the database can be queried when there is an emergency or for crisis response planning and not just showed to 911 call takers when an associated phone number calls 911. This system can also be used to contact people living at an address that is the target of swatting. The concept of a swatting registry was created by Sean Whitcomb when he was employed by the Seattle Police Department. During a swatting incident, a 911 dispatcher can type in an address. If there are known people associated with the address then the system automatically calls every phone number associated with the address. When someone answers their phone an automated system asks them if they are at the address and if so asks if an emergency. 
+
+### Instant dispatching
+
+As soon as an emergency call comes in with an address we will automatically dispatch first responders and unmanned aircraft with thermal imaging to the scene. We want to integrate with existing first responder computer aided dispatch software by using an application programming language (API) or a screen scraper such as [SikuliX](http://sikulix.com/) The emergency.help app 
+
--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import requests
 import os
 from flask import Flask, request, jsonify, render_template, request, send_from_directory
-from form import SignupForm, SigninForm
 import random
 import string
 import re
@@ -35,25 +34,56 @@ class Person(db.Model):
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
     first_name = db.Column(db.String())
-    middle_name = db.Column(db.String(), null=True)
+    middle_name = db.Column(db.String(), nullable=True)
     last_name = db.Column(db.String())
-    birthdate = db.Column(db.DateTime())
-    current_address_1 = db.Column(db.String(), null=True)
-    current_address_2 = db.Column(db.String(), null=True)
-    current_address_city = db.Column(db.String(), null=True)
-    current_address_state = db.Column(db.String(), null=True)
-    current_address_zip_code = db.Column(db.String(), null=True)
-    current_gps_location = db.Column(Geometry('POINT'), null=True)
-    social_security_number = db.Column(db.String(), null=True)
-    drivers_license_or_state_id_state = db.Column(db.String(), null=True)
-    drivers_license_or_state_id_number = db.Column(db.String(), null=True)
-    height = db.Column(db.Float()) # in centimeters
-    weight = db.Column(db.Interger())
-    contacts = db.Column(JSON(), null=True) # keys: name, relationship_to_person, phone_numbers (keys: phone_number, type), address
-    audit_trail = db.Column(JSON(), null=True) # log all access to the person's data (keys: email_address, ip, datetime, did_what, reason_for_access, agency, CAD_number, RMS_number)
-    incidents = db.Column(JSON(), null=True)
-    
+    aliases = db.Column(JSON(), nullable=True)
+    photographs = db.Column(JSON(), nullable=True)
+    date_of_birth = db.Column(db.DateTime())
+    persons_phone_numbers = db.Column(JSON(), nullable=True)
+    current_physical_living_address_1 = db.Column(db.String(), nullable=True)
+    current_physical_living_address_2 = db.Column(db.String(), nullable=True)
+    current_physical_living_address_city = db.Column(db.String(), nullable=True)
+    current_physical_living_address_state = db.Column(db.String(), nullable=True)
+    current_physical_living_address_zip_code = db.Column(db.String(), nullable=True)
+    past_physical_living_addresses = db.Column(JSON(), nullable=True)
+    locations_frequent = db.Column(JSON(), nullable=True)
+    current_gps_location = db.Column(Geometry('POINT'), nullable=True)
+    vehicles = db.Column(JSON(), nullable=True)
+    social_security_number_salt = db.Column(db.String(), nullable=True)
+    social_security_number_hash = db.Column(db.String(), nullable=True)
+    drivers_license_or_state_id_state = db.Column(db.String(), nullable=True)
+    drivers_license_or_state_id_number = db.Column(db.String(), nullable=True)
+    height = db.Column(db.Float(), nullable=True) # in centimeters
+    weight = db.Column(db.Integer(), nullable=True)
+    eye_color = db.Column(db.String(), nullable=True)
+    hair_color = db.Column(db.String(), nullable=True)
+    race = db.Column(db.String(), nullable=True)
+    tattoos = db.Column(JSON(), nullable=True)
+    scars = db.Column(db.String(), nullable=True)
+    marks = db.Column(db.String(), nullable=True)
+    contacts = db.Column(JSON(), nullable=True) # keys: name, relationship_to_person, phone_numbers (keys: phone_number, type), address
+    audit_trail = db.Column(JSON(), nullable=True) # log all access to the person's data (keys: email_address, ip, datetime, did_what, reason_for_access, agency, CAD_number, RMS_number)
+    incidents = db.Column(JSON(), nullable=True)
+    allergies = db.Column(JSON(), nullable=True)
+    medical_conditions = db.Column(JSON(), nullable=True)
+    mental_health_conditions = db.Column(JSON(), nullable=True)
+    languages = db.Column(JSON(), nullable=True)
+    medications = db.Column(JSON(), nullable=True)
+    appointments = db.Column(JSON(), nullable=True)
+    safety_information = db.Column(db.String(), nullable=True)
+    deescalation_instructions = db.Column(db.String(), nullable=True)
+    documents = db.Column(JSON(), nullable=True)
 
+@app.route("/static/<path:path>")
+def send_static(path):
+    return send_from_directory("static", path)
+
+@app.route("/for_first_responders", methods=["GET"])
+def first_responder_home():
+    return render_template(
+        "for_first_responders.html",
+        polly_api_key=os.getenv('POLLY_API_KEY')
+    )
 
 if __name__ == "__main__":
     app.run(port=9000, debug=True)
